2d8e1004384a77b9044a0331bdcabd5826f1fbd9
DatSplit <nielsteunissen@hotmail.nl>
2024-09-18 00:33:51 +0200

    parse

--- kafka/assignment/custom_consumer.py
+++ kafka/assignment/custom_consumer.py
@@ -15,7 +15,7 @@ experiment_started_schema = {
 }
 
 
-experiment_config_schema = {
+experiment_config_schema = parse({
     "type": "record", 
     "name": "ExperimentConfig", 
     "fields": [
@@ -46,7 +46,7 @@ experiment_config_schema = {
             } 
         }
     ]
-}
+})
 
 stabilization_started_schema = parse('''
 {
