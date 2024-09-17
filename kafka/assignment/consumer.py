37a87b2035208b6946c492b2002c3eae5dbafa98
DatSplit <nielsteunissen@hotmail.nl>
2024-09-17 22:33:25 +0200

    Add custom consumer

---
+++ kafka/assignment/consumer.py
@@ -0,0 +1,48 @@
+import signal
+import click 
+import random
+
+from confluent_kafka import Consumer
+
+
+def signal_handler(sig, frame):
+    print('EXITING SAFELY!')
+    exit(0)
+
+signal.signal(signal.SIGTERM, signal_handler)
+# Add de-serilizer to the consumer
+c = Consumer({
+    'bootstrap.servers': '13.60.146.188:19093',
+    'group.id': f"{random.random()}",
+    'auto.offset.reset': 'earliest',
+    'enable.auto.commit': 'true',
+    'security.protocol': 'SSL',
+    'ssl.ca.location': './auth/ca.crt',
+    'ssl.keystore.location': './auth/kafka.keystore.pkcs12',
+    'ssl.keystore.password': 'cc2023',
+    'ssl.endpoint.identification.algorithm': 'none',
+
+})
+
+@click.command()
+@click.argument('topic')
+def consume(topic: str): 
+    c.subscribe(
+        [topic], 
+        on_assign=lambda _, p_list: print(p_list)
+    )
+
+    num_events = 0
+    while True:
+        msg = c.poll(1.0)
+        if msg is None:
+            continue
+        if msg.error():
+            print("Consumer error: {}".format(msg.error()))
+            continue
+        num_events += 1
+        if num_events % 1000 == 0:
+            print(num_events)
+        print(msg.value())
+
+consume()
