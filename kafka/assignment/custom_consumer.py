bf42f9ce5267ec172ae66a935e0565403f5d18fe
DatSplit <nielsteunissen@hotmail.nl>
2024-09-18 09:49:53 +0200

    c

--- kafka/assignment/custom_consumer.py
+++ kafka/assignment/custom_consumer.py
@@ -116,7 +116,7 @@ print("Consumer created")
 def decode_avro_message(message, schema):
     # Extract the schema
     schema_start = message.index(b'{"type":"record"')
-    schema_end = message.index(b'}\x00', schema_start) + 1
+    schema_end = message.index(b'}\x00', schema_start)
     schema_json = message[schema_start:schema_end]
     # Parse the schema
     #schema = avro.schema.parse(schema_json)
@@ -125,7 +125,7 @@ def decode_avro_message(message, schema):
     reader = DatumReader(schema)
 
     # Create a BinaryDecoder for the actual data
-    data_start = schema_end + 1  # Skip the sync marker
+    data_start = schema_end  # Skip the sync marker
     decoder = BinaryDecoder(io.BytesIO(message[data_start:]))
     print(message[data_start:])
     # Read the data
